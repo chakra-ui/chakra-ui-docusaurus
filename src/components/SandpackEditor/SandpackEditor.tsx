47b3b339d98c3f4c2cb96dab7cdff655987573b2
Andrej Acevski <aacevski@gmail.com>
2022-10-27 13:02:10 +0400

    feat: adding styles to sandpack, adding sandpack examples to recipes

--- src/components/SandpackEditor/SandpackEditor.tsx
+++ src/components/SandpackEditor/SandpackEditor.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { Sandpack } from "@codesandbox/sandpack-react";
+import React from 'react';
+import { Box } from "@chakra-ui/react";
+import { SandpackCodeEditor, SandpackLayout, SandpackPreview, SandpackProvider } from "@codesandbox/sandpack-react";
 import { useColorMode } from '@docusaurus/theme-common';
+
 import { createFileMap } from "./createFileMap";
 
 const SandpackEditor = ({
@@ -11,17 +13,13 @@ const SandpackEditor = ({
   dependencies: { [key: string]: string };
 }) => {
   const files = createFileMap(children);
-
   const { colorMode } = useColorMode();
 
   return (
-    <Sandpack
+    <SandpackProvider
       template="react-ts"
       files={files}
-      theme={colorMode === "dark" ? "dark" : "light"}
-      options={{
-        showNavigator: true,
-      }}
+      theme={colorMode === 'dark' ? 'dark' : 'light'}
       customSetup={{
         dependencies: {
           '@chakra-ui/react': 'latest',
@@ -29,9 +27,28 @@ const SandpackEditor = ({
           'framer-motion': 'latest',
           '@emotion/react': 'latest',
           ...dependencies
-        }
+        },
       }}
-    />
+    >
+      <Box
+        as={SandpackLayout}
+        sx={{
+          '--sp-layout-height': 'auto',
+        }}
+      >
+        <SandpackCodeEditor
+          showLineNumbers
+          style={{
+            maxHeight: '500px',
+            minWidth: '400px',
+          }}
+        />
+        <Box
+          as={SandpackPreview}
+          minHeight='350px'
+        />
+      </Box>
+    </SandpackProvider>
   );
 };
 
