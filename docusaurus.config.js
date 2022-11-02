22929a5b12f386aa1ad9c71d900531d43d587b6d
Johann Ehlers <dev@johann-ehlers.eu>
2022-11-02 21:50:12 +0100

    feat: make categories collapse when opening sibblings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      docs: {
+        sidebar: {
+          autoCollapseCategories: true,
+        },
+      },
       navbar: {
         logo: {
           alt: "Chakra UI Logo",
