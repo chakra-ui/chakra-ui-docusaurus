9629ef9c31d154d03e93c6e0fac2cca495caebdc
Andrej Acevski <andrej@halicea.com>
2022-11-12 21:08:53 +0100

    feature: fixing styling + updating panda

--- src/components/component-overview/component-overview-card.tsx
+++ src/components/component-overview/component-overview-card.tsx
@@ -17,24 +17,32 @@ const ComponentOverviewCard = ({ url, title, imageUrl }: Props) => {
         textDecoration: 'none !important'
       })}
     >
-      <chakra.div
-        height="full"
-        borderRadius="lg"
-        borderColor={{ base: 'gray.200', dark: 'gray.700' }}
-        borderWidth="1px"
-        borderStyle="solid"
-        overflow="hidden"
-        transition='box-shadow 0.1s ease-out'
-        role="group"
-        color={{
-          base: 'black',
-          dark: 'white'
-        }}
-        background={{ base: 'inherit', dark: 'rgba(255, 255, 255, 0.04)' }}
-        shadow={{
-          base: 'none',
-          hover: 'md',
-        }}>
+      <div
+        className={css({
+          height: '100%',
+          borderRadius: 'lg',
+          borderColor: {
+            base: 'gray.200',
+            dark: 'gray.700'
+          },
+          borderWidth: "1px",
+          borderStyle: "solid",
+          overflow: "hidden",
+          transition: 'box-shadow 0.1s ease-out',
+          color: {
+            base: 'gray.900',
+            dark: 'gray.50'
+          },
+          background: {
+            base: 'inherit',
+            dark: 'rgba(255, 255, 255, 0.04)'
+          },
+          shadow: {
+            base: 'none',
+            hover: 'md'
+          }
+        })}
+      >
         <chakra.div display="flex" overflow="hidden" bg="gray.100">
           <chakra.img
             src={imageUrl}
@@ -46,8 +54,8 @@ const ComponentOverviewCard = ({ url, title, imageUrl }: Props) => {
         <chakra.p fontSize="md" fontWeight="semibold" px='1rem' py='0.75rem' margin='0 !important'>
           {title}
         </chakra.p>
-      </chakra.div>
-    </a>
+      </div>
+    </a >
   )
 }
 
